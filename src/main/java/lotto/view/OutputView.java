9063e408f3619c120e190d217698678d00475bca
박재중 <jjp910123@gmail.com>
2025-11-03 02:06:56 +0900

    feat: 구입한 로또 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,6 +24,10 @@ public class OutputView {
     }
 
     public static void displayLottos(List<Lotto> lottos) {
-
+        System.out.println(lottos.size()+"개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+        System.out.println();
     }
 }
