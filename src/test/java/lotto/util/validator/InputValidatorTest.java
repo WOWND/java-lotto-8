f762f497fda593891fcacdb42a06eb23286d1e67
박재중 <jjp910123@gmail.com>
2025-11-03 22:25:12 +0900

    test: 입력 테스트

--- src/test/java/lotto/util/validator/InputValidatorTest.java
+++ src/test/java/lotto/util/validator/InputValidatorTest.java
@@ -3,22 +3,32 @@ package lotto.util.validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("InputValidator 테스트")
-public class InputValidatorTest {
+class InputValidatorTest {
+    
     @Test
-    void 빈_문자열_입력() {
-        assertThrows(IllegalArgumentException.class,
-                () -> InputValidator.validateNotEmpty(null));
-
-        assertThrows(IllegalArgumentException.class,
-                () -> InputValidator.validateNotEmpty(""));
+    void 정상_입력() {
+        assertDoesNotThrow(() -> InputValidator.validateNotEmpty("1000"));
     }
 
     @Test
-    void 공백만_입력() {
-        assertThrows(IllegalArgumentException.class,
-                () -> InputValidator.validateNotEmpty("  "));
+    void null_입력_예외() {
+        assertThatThrownBy(() -> InputValidator.validateNotEmpty(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 빈_문자열_입력_예외() {
+        assertThatThrownBy(() -> InputValidator.validateNotEmpty(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 공백만_입력_예외() {
+        assertThatThrownBy(() -> InputValidator.validateNotEmpty("   "))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
