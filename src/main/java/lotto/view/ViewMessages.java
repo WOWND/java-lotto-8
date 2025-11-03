e076f603a05b460d834d28190b34bf0ea09cbea9
박재중 <jjp910123@gmail.com>
2025-11-03 13:18:53 +0900

    refactor: 상수를 통해 출력하도록 변경

--- src/main/java/lotto/view/ViewMessages.java
+++ src/main/java/lotto/view/ViewMessages.java
@@ -4,6 +4,7 @@ public class ViewMessages {
     public static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    public static final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.";
     public static final String LOTTO_RESULT_MESSAGE = "당첨 통계\n---";
     public static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
